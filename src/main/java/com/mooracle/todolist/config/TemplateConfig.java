79e4a249885f80fb631cbccd37a5ceb15124cbcb
mooracle <yanuar.prakosa@gmail.com>
2018-05-18 16:24:55 +0700

    entry 12: integrating user data by adding security dialect in template config

--- src/main/java/com/mooracle/todolist/config/TemplateConfig.java
+++ src/main/java/com/mooracle/todolist/config/TemplateConfig.java
@@ -2,9 +2,14 @@ package com.mooracle.todolist.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
 import org.thymeleaf.spring4.SpringTemplateEngine;
 import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+
+/** ENTRY LIST
+ *  ENTRY 12: Integrating User Data  Into Our Application.
+ * */
 
 @Configuration
 public class TemplateConfig {
@@ -19,11 +24,19 @@ public class TemplateConfig {
         return templateResolver;
     }
 
-    //next the template engine
+    /** next the template engine
+     *   Entry 12-1: adding the thymeleaf extras for spring security
+     *      insert the Spring Security dialect
+     *   Note: the main reason we use Java based config for the template instead of XML based config is to add this
+     *      functions such as dialects. This dialect then can be accessed in every html templates in the resources
+     *      directory. One we interested on is the layout template which will render the username in the nav bar
+     *
+     * */
     @Bean
     public SpringTemplateEngine templateEngine() {
         final SpringTemplateEngine springTemplateEngine = new SpringTemplateEngine();
         springTemplateEngine.addTemplateResolver(templateResolver());
+        springTemplateEngine.addDialect(new SpringSecurityDialect());//this is the added security dialect
         return springTemplateEngine;
     }
 
